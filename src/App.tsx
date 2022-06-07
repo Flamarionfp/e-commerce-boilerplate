0bca79434ea378cdd00c3a546bda3144e9cc851a
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-06-06 22:20:23 -0300

    chore: setup font familly and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import theme from './theme';
+import GlobalStyle from './theme/globalStyle';
 import { store } from './redux/store';
 import Routes from './routes';
 
@@ -8,6 +9,7 @@ const App = function () {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Routes />
       </ThemeProvider>
     </Provider>
